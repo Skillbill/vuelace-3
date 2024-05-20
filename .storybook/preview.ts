f8e3b39710d2210ed7a4153804376e29d9245c43
Biagio Ruggiero <biagioruggiero93@gmail.com>
2024-05-20 11:50:56 +0200

    Clean up preview from sorting

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -18,11 +18,6 @@ setup((app) => {
 
 const preview: Preview = {
   parameters: {
-    options: {
-      storySort: {
-        order: ['Introduction', 'Contribuiting']
-      }
-    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
