777156fc810785c0c6009a96829501b4efb032a6
Biagio Ruggiero <biagioruggiero93@gmail.com>
2024-05-23 17:46:21 +0200

    vuelace-logo in introduction

---
+++ .storybook/vuelace.ts
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create'
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Vuelace',
+  brandUrl: 'https://skillbill.github.io/vuelace-3/',
+  brandTarget: '_self'
+})
