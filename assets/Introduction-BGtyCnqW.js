b8f4876cd30d04c3cbf9576b7f89f6b0ba8862ad
pzocchi <pierantonio.zocchi@gmail.com>
2024-05-23 15:54:11 +0000

    Deploying to docs from @ Skillbill/vuelace-3@b6b5aac818203c022055b7654f234d5c16a94819 🚀

---
+++ assets/Introduction-BGtyCnqW.js
@@ -0,0 +1,40 @@
+import{r as p}from"./index-B3ehnkiM.js";import{useMDXComponents as c}from"./index-rbP-kYFP.js";import"./_commonjsHelpers-Cpj98o6Y.js";var d={exports:{}},t={};/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var f=p,x=Symbol.for("react.element"),m=Symbol.for("react.fragment"),b=Object.prototype.hasOwnProperty,j=f.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,w={key:!0,ref:!0,__self:!0,__source:!0};function u(n,e,r){var o,i={},s=null,a=null;r!==void 0&&(s=""+r),e.key!==void 0&&(s=""+e.key),e.ref!==void 0&&(a=e.ref);for(o in e)b.call(e,o)&&!w.hasOwnProperty(o)&&(i[o]=e[o]);if(n&&n.defaultProps)for(o in e=n.defaultProps,e)i[o]===void 0&&(i[o]=e[o]);return{$$typeof:x,type:n,key:s,ref:a,props:i,_owner:j.current}}t.Fragment=m;t.jsx=u;t.jsxs=u;d.exports=t;var l=d.exports;function h(n){const e={a:"a",h1:"h1",h2:"h2",p:"p",...c(),...n.components};return l.jsxs(l.Fragment,{children:[l.jsx("style",{children:`
+  .sbdocs.sbdocs-wrapper {
+    padding: 0 20px;
+  }
+`}),`
+`,l.jsx("div",{style:{display:"flex",gap:"16px",alignItems:"center",justifyContent:"end",width:"100%",position:"fixed",top:0,right:20,zIndex:50},children:l.jsxs("div",{style:{display:"flex",gap:"16px",alignItems:"center",padding:"12px",backgroundColor:"white"},children:[l.jsx("a",{href:"https://www.npmjs.com/package/@skillbill/vuelace-3",children:l.jsx("img",{style:{height:"36px"},src:"assets/vuelace-logo.svg",alt:"GitHub"})}),l.jsx("a",{href:"https://www.github.com/Skillbill/vuelace-3",children:l.jsx("img",{style:{height:"36px"},src:"assets/github-mark.svg",alt:"GitHub"})}),l.jsx("a",{href:"https://www.skillbill.it",children:l.jsx("img",{style:{height:"36px"},src:"assets/logo_skillbill_00.svg",alt:"Skillbill"})})]})}),`
+`,l.jsx(e.h1,{id:"introduction",children:"Introduction"}),`
+`,l.jsxs(e.p,{children:[l.jsx(e.a,{href:"https://www.github.com/Skillbill/vuelace-3",rel:"nofollow",children:"Vuelace-3"}),` is a UI component library for Vue,
+built on top of `,l.jsx(e.a,{href:"https://shoelace.style/",rel:"nofollow",children:"Shoelace"})," and ",l.jsx(e.a,{href:"https://primevue.org/",rel:"nofollow",children:"PrimeVue"}),"."]}),`
+`,l.jsx(e.p,{children:`The scope is to have a friendly components wrapper for the Shoelace web components ready to use in Vue.
+We also included some utilities to use icons comfortably and fulfill missing components with PrimeVue's ones.`}),`
+`,l.jsx(e.h2,{id:"details",children:"Details"}),`
+`,l.jsx(e.p,{children:"If you want to take a look at the components, you can find them on the left panel."}),`
+`,l.jsx(e.p,{children:`If you need to build a site using Vue and you are looking for a ready-to-use components for crud,
+forms, and so on, you are in the right place.`}),`
+`,l.jsx(e.h2,{id:"quick-start",children:"Quick start"}),`
+`,l.jsxs(e.p,{children:["Vuelace-3 is available at ",l.jsx(e.a,{href:"https://www.npmjs.com/package/@skillbill/vuelace-3",rel:"nofollow",children:"npm"}),`.
+It is an easy-to-use vue3 library,
+you can find every updated detail in the `,l.jsx(e.a,{href:"https://github.com/Skillbill/vuelace-3/blob/main/README.md",rel:"nofollow",children:"README"})]}),`
+`,l.jsxs(e.p,{children:["You can also find a video tutorial ",l.jsx(e.a,{href:"https://youtu.be/uYN2l9t50bc",rel:"nofollow",children:"here"})]}),`
+`,l.jsx(e.h2,{id:"support",children:"Support"}),`
+`,l.jsxs(e.p,{children:[`Do you have questions, suggestions or do you need help?
+Please feel free to `,l.jsx(e.a,{href:"https://github.com/Skillbill/vuelace-3/issues/new/choose",rel:"nofollow",children:"open"})," an issue on GitHub!"]}),`
+`,l.jsx(e.h2,{id:"contributing",children:"Contributing"}),`
+`,l.jsxs(e.p,{children:["There are some guidelines in ",l.jsx(e.a,{href:"https://github.com/Skillbill/vuelace-3/blob/main/CONTRIBUTING.md",rel:"nofollow",children:"CONTRIBUTING"})]}),`
+`,l.jsx(e.h2,{id:"license",children:"License"}),`
+`,l.jsxs(e.p,{children:["The whole library is under ",l.jsx(e.a,{href:"https://github.com/Skillbill/vuelace-3/blob/main/LICENSE",rel:"nofollow",children:"MIT License"})]}),`
+`,l.jsx(e.h2,{id:"versioning",children:"Versioning"}),`
+`,l.jsxs(e.p,{children:["We use the standard ",l.jsx(e.a,{href:"https://semver.org/",rel:"nofollow",children:"SemVer"})]}),`
+`,l.jsx(e.h2,{id:"about",children:"About"}),`
+`,l.jsxs(e.p,{children:["Vuelace-3 is a library built for internal needs by ",l.jsx(e.a,{href:"https://www.skillbill.it",rel:"nofollow",children:"Skillbill"}),`
+and made open source to allow anyone to use it or contribute to it.`]})]})}function k(n={}){const{wrapper:e}={...c(),...n.components};return e?l.jsx(e,{...n,children:l.jsx(h,{...n})}):h(n)}export{k as default};
