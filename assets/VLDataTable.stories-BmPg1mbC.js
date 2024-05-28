be1c76b041bf1547f7134f2cc4c704baad7baf00
pzocchi <pierantonio.zocchi@gmail.com>
2024-05-28 15:00:25 +0000

    Deploying to docs from @ Skillbill/vuelace-3@1e054c21ef4806d4bfe66ab7343c0b31ef6cb676 🚀

---
+++ assets/VLDataTable.stories-BmPg1mbC.js
@@ -0,0 +1,30 @@
+import{c as r}from"./VLTextArea-DBzmJiQC.js";import{_ as s}from"./ActiveCell-DwQuZy9X.js";import{s as m}from"./vue.esm-bundler-BYZbTIRI.js";import"./api.esm-C3jrVNCq.js";import"./main-CZ2WSQaV.js";const i=()=>new Array(40).fill(0).map((l,e)=>({username:`user${e}`,firstName:`Name${e}`,lastName:`LastName${e}`,active:e%2,activation_date:"2021-01-01",expiration_date:"2025-12-31"})),_={title:"Components/Data Table",component:r,tags:["autodocs"],argTypes:{selectionMode:{control:"select",options:[void 0,"single","multiple"]}},args:{}},a={args:{columns:[{name:"Username",value:"username"},{name:"First name",value:"firstName",sortable:!0},{name:"Last name",value:"lastName"},{name:"Active",value:"active",component:m(s),componentProps:{trueColor:"text-yellow-500"}},{name:"Activation Date",value:"activation_date"},{name:"Expiration Date",value:"expiration_date"}],actions:[],items:i()}};var t,n,o;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    columns: [{
+      name: 'Username',
+      value: 'username'
+    }, {
+      name: 'First name',
+      value: 'firstName',
+      sortable: true
+    }, {
+      name: 'Last name',
+      value: 'lastName'
+    }, {
+      name: 'Active',
+      value: 'active',
+      component: shallowRef(ActiveComponent),
+      componentProps: {
+        trueColor: 'text-yellow-500'
+      }
+    }, {
+      name: 'Activation Date',
+      value: 'activation_date'
+    }, {
+      name: 'Expiration Date',
+      value: 'expiration_date'
+    }],
+    actions: [],
+    items: getItems()
+  }
+}`,...(o=(n=a.parameters)==null?void 0:n.docs)==null?void 0:o.source}}};const f=["Simple"];export{a as Simple,f as __namedExportsOrder,_ as default};
--- assets/VLTooltip.stories-BUhhX1I0.js
+++ assets/VLTooltip.stories-BUhhX1I0.js
@@ -1,4 +1,4 @@
-import{l as o}from"./VLTextArea-7PpHfbYC.js";import"./vue.esm-bundler-BYZbTIRI.js";import"./api.esm-C3jrVNCq.js";const i={title:"Components/Tooltip",component:o,tags:["autodocs"],render:a=>({components:{VLTooltip:o},setup(){return{args:a}},template:`
+import{l as o}from"./VLTextArea-DBzmJiQC.js";import"./vue.esm-bundler-BYZbTIRI.js";import"./api.esm-C3jrVNCq.js";const i={title:"Components/Tooltip",component:o,tags:["autodocs"],render:a=>({components:{VLTooltip:o},setup(){return{args:a}},template:`
       <VLTooltip v-bind="args">
         <span>{{args.default}}</span>
       </VLTooltip>
